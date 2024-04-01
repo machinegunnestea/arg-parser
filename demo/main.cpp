0b0e69bef256fc77c21610668c2ff48be32ae39b
Anastasia Kovalenko <100113835+machinegunnestea@users.noreply.github.com>
2024-04-01 15:45:48 +0300

    cmake-add-validator-hpp

--- demo/main.cpp
+++ demo/main.cpp
@@ -5,12 +5,12 @@ int main(int argc, const char** argv) {
 	args_parse::ArgsParser parser;
 
 	// Define arguments
-	args_parse::BoolArg help('h', "help");
+	//args_parse::BoolArg help('h', "help");
 	args_parse::IntArg number('g', "getNumber");
 	args_parse::StringArg output('o', "output");
 
 	// Add arguments to parser
-	parser.add(&help);
+	//parser.add(&help);
 	parser.add(&number);
 	parser.add(&output);
 
@@ -18,9 +18,9 @@ int main(int argc, const char** argv) {
 	parser.parse(argc, argv);
 
 	// if help was activated
-	if (help.isDefined()) {
-		parser.printHelp();
-	}
+	//if (help.isDefined()) {
+	//	parser.printHelp();
+	//}
 	// if output was activated
 	if (!output.value().empty()) {
 		std::cout << "Output value: " << output.value() << std::endl;
