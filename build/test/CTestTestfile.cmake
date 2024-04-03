152d9e9354184f72c5f95e3ffdd0a734d6a01fa8
Anastasia Kovalenko <100113835+machinegunnestea@users.noreply.github.com>
2024-04-03 14:08:49 +0300

    conan

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cpp/first/arg-parser/test
-# Build directory: D:/cpp/first/arg-parser/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/cpp/first/arg-parser/build/test/_unit_test_args_parse-b12d07c_include.cmake")
